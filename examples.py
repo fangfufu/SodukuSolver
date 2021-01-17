7b8a968116a3b2f988ffb6eaf1488dea83482be2
Fufu Fang <fangfufu2003@gmail.com>
2021-01-17 05:19:06 +0000

    Definitive working version - pre-documentation
    I think I will stop here, Sudoku is NP-complete. So you definitely
    cannot program an algorithm to make it go faster in those extreme
    cases. For now, I don't really want to think about how big the
    problem this is. It is clear that my program solves simple puzzles.

--- examples.py
+++ examples.py
@@ -5,11 +5,13 @@ Example sodoku puzzles to be tested
 
 @author: fangfufu
 """
-
+import time
 import numpy
 from sudoku import SudokuSolver
 
 if __name__ == '__main__':
+    #Step count: 43
+    #Stack length: 44
     christmas_puzzle_1 = numpy.array([
             [3, 9, 0, 0, 7, 0, 0, 0, 1],
             [0, 0, 0, 0, 1, 3, 4, 7, 2],
@@ -21,7 +23,8 @@ if __name__ == '__main__':
             [9, 5, 2, 3, 8, 0, 0, 0, 0],
             [7, 0, 0, 0, 5, 0, 0, 2, 8]
             ])
-            
+    #Step count: 43
+    #Stack length: 44        
     christmas_puzzle_2 = numpy.array([
             [4, 0, 8, 6, 0, 0, 0, 5, 0],
             [0, 0, 0, 0, 0, 8, 1, 2, 3],
@@ -60,6 +63,12 @@ if __name__ == '__main__':
             ])
     
     # http://www.mathsphere.co.uk/downloads/sudoku/10203-hard.pdf
+    
+    # check_identical_config=True
+    #Step count: 1140
+    #Tried length: 483
+    #Stack length: 56
+    #process_time: 129.753542497
     hard_puzzle_1 = numpy.array([
             [0, 0, 0, 0, 0, 0, 0, 0, 2],
             [0, 0, 0, 0, 0, 0, 9, 4, 0],
@@ -72,6 +81,16 @@ if __name__ == '__main__':
             [4, 0, 8, 5, 0, 0, 3, 6, 0]
             ])
     
+    
+    #check_identical_config=False 
+    #Step count: 46068
+    #Tried length: 0
+    #Stack length: 51
+    
+    #check_identical_config=True     
+    #Step count: 1149
+    #Tried length: 489
+    #Stack length: 51
     hard_puzzle_10 = numpy.array([
             [0, 0, 3, 0, 0, 7, 0, 6, 0],
             [0, 0, 7, 8, 0, 0, 2, 0, 0],
@@ -83,6 +102,33 @@ if __name__ == '__main__':
             [0, 7, 6, 3, 9, 4, 0, 0, 0],
             [0, 0, 4, 0, 0, 5, 0, 8, 0]
             ])
-    solver = SudokuSolver(hard_puzzle_1)
+    
+    ## http://www.mathsphere.co.uk/downloads/sudoku/10204-fiendish.pdf
+    fiendish_1 = numpy.array([
+            [9, 5, 0, 0, 0, 1, 0, 0, 2],
+            [6, 3, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 8, 0, 6, 0, 0, 0, 7],
+            [0, 0, 0, 0, 0, 0, 5, 0, 0],
+            [0, 6, 1, 7, 0, 9, 0, 0, 0],
+            [0, 0, 2, 0, 4, 0, 0, 0, 8],
+            [0, 9, 0, 0, 0, 0, 0, 0, 5],
+            [0, 1, 0, 0, 5, 6, 4, 8, 0],
+            [0, 8, 0, 0, 1, 7, 0, 0, 6]
+            ])
+    
+    fiendish_5 = numpy.array([
+            [0, 0, 7, 3, 0, 0, 2, 0, 5],
+            [0, 0, 4, 0, 0, 9, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 7, 6, 0],
+            [4, 0, 0, 0, 0, 0, 0, 7, 8],
+            [0, 1, 0, 5, 0, 0, 0, 0, 2],
+            [0, 0, 8, 0, 0, 6, 1, 0, 0],
+            [0, 2, 0, 0, 0, 1, 4, 0, 0],
+            [0, 0, 0, 8, 0, 0, 0, 3, 7],
+            [0 ,0, 0, 0, 0, 5, 0, 0, 0]
+            ])
+    
+    solver = SudokuSolver(fiendish_5, check_identical_config=True)
     solver.solve()
     print(solver)
+    print(time.process_time())
--- sudoku.py
+++ sudoku.py
@@ -9,26 +9,26 @@ import numpy
 import math
 import sys
 
-class OutOfChoice(Exception):
-    ''' For use by SudokuConfig, when running out of choices'''
+class OutOfdecision(Exception):
+    ''' For use by SudokuConfig, when running out of decisions'''
     pass
 
 class InvalidConfiguration(ValueError):
     ''' For use by SudokuConfig, when the sudoku configuration is valid '''
     def __init__(self, val):
-        message = "Invalid Sudoku configuration."
+        message = "Invalid sudoku configuration."
         super().__init__(message, str(val))
 
 class SudokuConfig():
     '''
-    SudokuConfig -- Container for the state information for a Sudoku 
+    SudokuConfig -- Container for the state information for a sudoku 
     configuration
     
     Args:
         config: The configuration to generate the state information for.
         
     Attributes:
-        valid_choices: a 9x9x9 boolean numpy array indicating the valid choices
+        valid_decisions: a 9x9x9 boolean numpy array indicating the valid decisions
             for each square
         priority list: a list of tuples (row, col), indicating which squares 
             should be filled first.
@@ -37,15 +37,14 @@ class SudokuConfig():
     '''
     
     def __init__(self, config):
-        config = numpy.array(config)
         if config.shape != (9, 9):
                 raise ValueError("Invalid input configuration dimension")
         self.config = config.copy()
-        self.valid_choices = self.__gen_valid_choices()
+        self.valid_decisions = self.__gen_valid_decisions_array()
         self.priority_list = self.__gen_priority_list()
         self.current_level = 0
-        self.current_selection = 0
-        self.choice_list = []
+        self.current_decision = 0
+        self.decision_cache = []
         
     def __eq__(self, other):
         return numpy.array_equal(self.config, other.config)
@@ -56,8 +55,8 @@ class SudokuConfig():
     def __next__(self):
         '''Make the class iterable'''
         try:
-            return self.gen_config(len(self.choice_list))
-        except OutOfChoice:
+            return self.gen_config(len(self.decision_cache))
+        except OutOfdecision:
             raise StopIteration
             
     def __repr__(self):
@@ -94,7 +93,7 @@ class SudokuConfig():
 
     def is_valid(self): 
         ''' 
-        Check if a Sudoku configuration is valid 
+        Check if a sudoku configuration is valid 
         
         Returns:
              0: complete and valid configuration
@@ -114,8 +113,8 @@ class SudokuConfig():
         
         return total
         
-    def __gen_valid_choices(self):
-        ''' Generate the valid choices array '''
+    def __gen_valid_decisions_array(self):
+        ''' Generate the valid decisions array '''
         # Initially every number is possible in every square, as we discover
         # existing numbers, they get masked out. 
         # We use the value in the puzzle to do indexing directly, 
@@ -131,17 +130,17 @@ class SudokuConfig():
                     col_arr[j, num] = False
                     square_arr[math.floor(i/3), math.floor(j/3), num] = False
                     
-        # Calculate the valid choices
-        valid_choices = numpy.full((9, 9, 9), True)
+        # Calculate the valid decisions
+        valid_decisions = numpy.full((9, 9, 9), True)
         for i in range(0,9):
             for j in range(0,9):
-                valid_choices[i,j] = row_arr[i] & col_arr[j] & \
+                valid_decisions[i,j] = row_arr[i] & col_arr[j] & \
                 square_arr[math.floor(i/3), math.floor(j/3)]
-        return valid_choices
+        return valid_decisions
                 
     def __gen_priority_list(self):
-        ''' Generate the priority list for a Sudoku configuration '''
-        self.priority2d = numpy.sum(self.valid_choices, 2)
+        ''' Generate the priority list for a sudoku configuration '''
+        self.priority2d = numpy.sum(self.valid_decisions, 2)
         prioritytbl = []
         for i in range(0, 9):
             for j in range(0, 9):
@@ -150,74 +149,81 @@ class SudokuConfig():
         return [(x[0], x[1]) for x in sorted(prioritytbl, key=lambda x: x[2]) 
                               if x[2] != 0]
     
-    def __gen_choice(self, n):
+    def __gen_decision(self, n):
         '''
-        Generate the nth choice from a priority list and valid choices pair
+        Generate the nth decision from a priority list and valid decisions pair
         
         Args: 
-            n: the nth choice to generate
+            n: the nth decision to generate
             
         Returns:
-            The nth choice at the current configuration in the format of 
+            The nth decision at the current configuration in the format of 
             (row, col, num), so square at (row, col) needs to be filled with 
-            num. (0, 0, 0) will be returned if a choice cannot be generated.
+            num. (0, 0, 0) will be returned if a decision cannot be generated.
         '''
         
-        # We have cached the previous choice
-        if n < len(self.choice_list):
-            return self.choice_list[n]
+        # We have cached the previous decisions for backtracking
+        if n < len(self.decision_cache):
+            return self.decision_cache[n]
         
-        # Generate new choices
+        # Generate new decisions
         for i in range(self.current_level, len(self.priority_list)):
             self.current_level = i
             coord = self.priority_list[i]
-            choices = self.valid_choices[coord]
-            for j in range(self.current_selection, len(choices)):
-                self.current_selection = j
-                if choices[j]:
-                    self.choice_list.append((coord[0], coord[1], j + 1))
-                    if n == (len(self.choice_list) - 1):
+            decisions = self.valid_decisions[coord]
+            for j in range(self.current_decision, len(decisions)):
+                self.current_decision = j
+                if decisions[j]:
+                    self.decision_cache.append((coord[0], coord[1], j + 1))
+                    if n == (len(self.decision_cache) - 1):
                         # Manually increase the loop counter by 1, because we
                         # are exiting
-                        self.current_selection += 1
-                        return self.choice_list[-1]       
+                        self.current_decision += 1
+                        return self.decision_cache[-1]       
                 
         # We have reached the end without getting a solution 
         return (0, 0, 0)
     
     def gen_config(self, n):
         '''
-        Generate a new Sudoku configuration based on the nth choice
+        Generate a new sudoku configuration based on the nth decision
         
         Args:
             n: the nth new configuration to generate
             
         Returns:
-            The nth new Sudoku configuration
+            The nth new sudoku configuration
         '''
-        choice = self.__gen_choice(n)
-        if choice == (0, 0, 0):
-            raise OutOfChoice();
+        decision = self.__gen_decision(n)
+        if decision == (0, 0, 0):
+            raise OutOfdecision();
         new_config = numpy.copy(self.config)
-        new_config[choice[0], choice[1]] = choice[2]
+        new_config[decision[0], decision[1]] = decision[2]
         return SudokuConfig(new_config)
         
     
 class SudokuSolver():
     '''
-    SudokuSolver -- a Sudoku solver that uses numpy array
+    SudokuSolver -- a sudoku solver that uses numpy array
 
     Args:
         input_array: the puzzle that needs to be solved
+        limit: the maximun number of step to attempt before terminating
+        check_identical_config: check whether the newly generated configuration
+            is identical to a previously generated configuration. 
         
     Attributes:
-        configs: the Sudoku configuration stack
-        solution: the Sudoku solution
+        config_stack: the sudoku configuration stack
+        tried_config: the sudoku configuration we have tried
+        solution: the sudoku solution
         self.limit: the maximum step count before aborting
         n: the number of steps taken
     '''
 
-    def __init__(self, input_array, limit=sys.maxsize):
+    def __init__(self, 
+                 input_array, 
+                 limit=sys.maxsize, 
+                 check_identical_config=True):
         ''' Constructor '''
         self.config_init = SudokuConfig(input_array)
         validity = self.config_init.is_valid()
@@ -225,46 +231,54 @@ class SudokuSolver():
             raise ValueError("The input puzzle has already been solved.")
         
         # The configurations we have tried
-        self.configs = [self.config_init]
+        self.config_stack = [self.config_init]
+        self.tried_config = []
         self.solution = None
         self.limit = limit
-        self.n = 0
-    
-    def __len__(self):
-        return self.configs.__len__()
+        self.step = 0
+        self.check_identical_config = check_identical_config
     
     def __repr__(self):
         return "<SudokuSolver: len:" + str(self.__len__()) + ">"
     
     def __str__(self):
         return "--- SudokuSolver --- \n" + \
-            "Step count: " + str(self.n) + "\n" + \
-            "Stack length: " + str(self.__len__()) + "\n" + \
-            self.configs[-1].__str__() + "\n"
+            "Step count: " + str( self.step) + "\n" + \
+            "Tried length: " + str(self.tried_config.__len__()) + "\n" + \
+            "Stack length: " + str(self.config_stack.__len__()) + "\n" + \
+            self.config_stack[-1].__str__() + "\n"
     
     def __getitem__(self, i):
-        return self.configs[i]
+        return self.config_stack[i]
     
     def solve(self):
         '''
-        Solve a Sudoku puzzle using depth-first search with backtracking
+        Solve a sudoku puzzle using depth-first search with backtracking
         '''
         # While we still have squares to fill, try and fill the squares
-        while self.configs[-1].is_valid() > 0 and self.n < self.limit:
-            if not (self.n % 10000):
+        while self.config_stack[-1].is_valid() > 0 and  self.step < self.limit:
+            if not ( self.step % 10000):
                 print(self)
             try:
-                next_config = next(self.configs[-1])
+                self.step += 1
+                next_config = next(self.config_stack[-1])
+                if not self.check_identical_config:
+                    self.config_stack.append(next_config)
+                else:
+                    if self.config_already_exists(next_config):
+                        continue
+                    else:
+                        self.tried_config.append(next_config)
+                        self.config_stack.append(next_config)
+                    
             except StopIteration:
-                self.configs.pop()
-            if not self.is_existing_config(next_config):
-                self.configs.append(next_config)
-            self.n += 1
-        self.solution = self.configs[-1]
+                self.config_stack.pop()
+
+        self.solution = self.config_stack[-1]
         return self.solution
 
-    def is_existing_config(self, this_config):
-            for i in range(-1, -len(self)-1, -1):
-                if this_config == self.configs[i]:
-                    return True
+    def config_already_exists(self, this_config):
+        for i in range(-1, -len(self.tried_config)-1, -1):
+            if this_config == self.tried_config[i]:
+                return True
                         
