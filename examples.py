59164b01c32f732af45ab66bbdf653b273ff8b5f
Fufu Fang <fangfufu2003@gmail.com>
2021-01-17 05:40:51 +0000

    added peter norvig's hardest puzzle

--- examples.py
+++ examples.py
@@ -10,8 +10,6 @@ import numpy
 from sudoku import SudokuSolver
 
 if __name__ == '__main__':
-    #Step count: 43
-    #Stack length: 44
     christmas_puzzle_1 = numpy.array([
             [3, 9, 0, 0, 7, 0, 0, 0, 1],
             [0, 0, 0, 0, 1, 3, 4, 7, 2],
@@ -23,8 +21,7 @@ if __name__ == '__main__':
             [9, 5, 2, 3, 8, 0, 0, 0, 0],
             [7, 0, 0, 0, 5, 0, 0, 2, 8]
             ])
-    #Step count: 43
-    #Stack length: 44        
+    
     christmas_puzzle_2 = numpy.array([
             [4, 0, 8, 6, 0, 0, 0, 5, 0],
             [0, 0, 0, 0, 0, 8, 1, 2, 3],
@@ -63,12 +60,6 @@ if __name__ == '__main__':
             ])
     
     # http://www.mathsphere.co.uk/downloads/sudoku/10203-hard.pdf
-    
-    # check_identical_config=True
-    #Step count: 1140
-    #Tried length: 483
-    #Stack length: 56
-    #process_time: 129.753542497
     hard_puzzle_1 = numpy.array([
             [0, 0, 0, 0, 0, 0, 0, 0, 2],
             [0, 0, 0, 0, 0, 0, 9, 4, 0],
@@ -81,16 +72,6 @@ if __name__ == '__main__':
             [4, 0, 8, 5, 0, 0, 3, 6, 0]
             ])
     
-    
-    #check_identical_config=False 
-    #Step count: 46068
-    #Tried length: 0
-    #Stack length: 51
-    
-    #check_identical_config=True     
-    #Step count: 1149
-    #Tried length: 489
-    #Stack length: 51
     hard_puzzle_10 = numpy.array([
             [0, 0, 3, 0, 0, 7, 0, 6, 0],
             [0, 0, 7, 8, 0, 0, 2, 0, 0],
@@ -116,6 +97,7 @@ if __name__ == '__main__':
             [0, 8, 0, 0, 1, 7, 0, 0, 6]
             ])
     
+    # This appears to be unsolvable... 
     fiendish_5 = numpy.array([
             [0, 0, 7, 3, 0, 0, 2, 0, 5],
             [0, 0, 4, 0, 0, 9, 0, 0, 0],
@@ -128,8 +110,25 @@ if __name__ == '__main__':
             [0 ,0, 0, 0, 0, 5, 0, 0, 0]
             ])
     
+    #https://norvig.com/top95solutions.html
+    # This seems to be Peter Norvig's hardest puzzle
+    #Step count: 11444
+    #Tried length: 4685
+    #Stack length: 58
+    norvig_0_19 = numpy.array([
+            [0, 0, 2, 4, 7, 0, 0, 5, 8],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 4, 0],
+            [0, 0, 0, 0, 2, 0, 0, 0, 9],
+            [5, 2, 8, 0, 9, 0, 4, 0, 0],
+            [0, 0, 9, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 3, 0],
+            [3, 0, 0, 0, 0, 7, 5, 0, 0],
+            [6, 8, 5, 0, 0, 2, 0, 0, 0]
+            ])
+    
     zeros = numpy.zeros([9, 9], int)
     
-    solver = SudokuSolver(zeros, check_identical_config=True)
+    solver = SudokuSolver(norvig_0_19, check_identical_config=False)
     solver.solve()
     print(solver)
